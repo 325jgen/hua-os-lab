f62ed6b4154807b0a014ae247d082640b123e0a4
Jorgen <jorgen.cani7@outlook.com>
2021-12-19 14:10:26 +0200

    Changed root_fs_size from showing total size to used size and now using bc instead of awk for the calculation

--- it22105_ex_3.sh
+++ it22105_ex_3.sh
@@ -8,16 +8,16 @@ then
 fi
 
 # Find size of the root filesystem and print it (returns a number in a human readble form)
-root_fs_size=$(df -h | grep -e "/\$" | awk '{print $4}')
+root_fs_size=$(df -h | grep -e "/\$" | awk '{print $3}')
 echo -e "Root filesystem size: $root_fs_size"
-# Since the human readable form of the size cannot be used to calculcate the percentage usage later in the script, we need to remove it 
-root_fs_size=$(echo "$root_fs_size" | tr 'a-zA-Z' ' ')
+# Since the human readable form of the size cannot be used to calculcate the percentage usage later in the script, we need to rerun the command without the -h flag 
+root_fs_size=$(df | grep -e "/\$" | awk '{print $3}')
 
 # Same as $root_fs_size but using du to calculcate home directory usage
-home_dir_size=$(du -h ~/ | tail -1 | awk '{print $1}')
+home_dir_size=$(du -h $HOME | tail -1 | awk '{print $1}')
 echo -e "Home directory size: $home_dir_size"
-home_dir_size=$(echo "$home_dir_size" | tr 'a-zA-Z' ' ')
+home_dir_size=$(du $HOME | tail -1 | awk '{print $1}')
 
 # Using awk to calculcate the percentage. The `print` command prints a float with many decimal points, but thre's only the need of the integer part of it. That's why `printf` needs to be used for this. 
-home_usage=$(echo "$home_dir_size $root_fs_size"| awk '{printf "%d\n", $1/$2 * 100}')
+home_usage=$(echo "scale=2; ($home_dir_size/$root_fs_size)*100" | bc -l) 
 echo -e "Home directory uses $home_usage% of /" 
